57f2435bcd4aee3c9ac202e221195001eadcb317
alimbayev303 <115586588+alimbayev303@users.noreply.github.com>
2023-11-29 20:51:22 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,84 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try {
+            System.out.println("Welcome to the Restricted Calculator!");
+            System.out.println("Enter your arithmetic expression or type 'exit' to exit:");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+            while (true) {
+                System.out.print("Expression: ");
+                String userInput = scanner.nextLine();
+
+                if (userInput.equalsIgnoreCase("exit")) {
+                    System.out.println("Exiting Calculator. Goodbye!");
+                    break;
+                }
+
+                int result = calculateExpression(userInput);
+                System.out.println("Result: " + result);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
         }
+    }
+
+    private static int calculateExpression(String expression) {
+        String[] parts = expression.split("\\s+");
+
+        if (parts.length == 3) {
+            int a = validateAndParse(parts[0]);
+            int b = validateAndParse(parts[2]);
+
+            switch (parts[1]) {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    return a / b;
+                default:
+                    throw new IllegalArgumentException("Invalid operator: " + parts[1]);
+            }
+        } else if (parts.length == 5) {
+            int a = validateAndParse(parts[0]);
+            int b = validateAndParse(parts[2]);
+            int c = validateAndParse(parts[4]);
+
+            switch (parts[1] + parts[3]) {
+                case "+-":
+                    return a + b - c;
+                case "-+":
+                    return a - b + c;
+                case "*-":
+                    return a * b - c;
+                case "/-":
+                    return a / b * c;
+                default:
+                    throw new IllegalArgumentException("Invalid operator: " + parts[1] + parts[3]);
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid expression format");
+        }
+    }
+
+    private static int validateAndParse(String input) {
+        int value;
+        try {
+            value = Integer.parseInt(input);
+            if (value < 1 || value > 10) {
+                throw new IllegalArgumentException("Numbers must be between 1 and 10 inclusive.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid number format: " + input);
+        }
+        return value;
     }
 }
